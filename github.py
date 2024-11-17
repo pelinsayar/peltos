fafab8b9b023b6c08f34345f7cd35f2a11fb5ba7
pelinsayar <peliinsayar@outlook.com>
2024-11-17 21:58:52 +0300

    Add files via upload

---
+++ github.py
@@ -0,0 +1,13 @@
+
+
+import streamlit as st
+
+st.title("Bisiklet Kiralama Tahmin Uygulaması")
+st.write("Bu uygulama canlı hava durumu verilerini kullanarak bisiklet kiralama tahminleri yapar!")
+
+
+
+
+
+
+
