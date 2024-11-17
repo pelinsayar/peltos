364751e20147d78e550e30d78a5dddfd1f903e1e
pelinsayar <peliinsayar@outlook.com>
2024-11-17 22:22:49 +0300

    Update github.py

--- github.py
+++ github.py
@@ -1,6 +1,12 @@
 
-
 import streamlit as st
+import joblib
+import requests
+import pandas as pd
+from datetime import datetime
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import urllib.request
 
 st.title("Bisiklet Kiralama Tahmin Uygulaması 🚲")
 st.markdown("""
@@ -8,9 +14,33 @@ Bu uygulama, İzmir için canlı hava durumu verilerini kullanarak bisiklet kira
 Aşağıdaki seçenekleri doldur ve tahminleri gör! 🌟
 """)
 
-git add models/bike_rentals_model.pkl  # Dosyayı ekle
-git commit -m "Model dosyasını eklendi"  # Commit mesajı
-git push  # GitHub'a yükle
+# GitHub'dan dosya indir
+model_url = "https://github.com/pelinsayar/peltos/blob/main/bike_rentals_model.pkl"
+model_data = requests.get(model_url).content
+
+# Modeli yükle
+with open("bike_rentals_model.pkl", "wb") as f:
+    f.write(model_data)
+
+final_model = joblib.load("bike_rentals_model.pkl")
+
+
+# Kullanıcıdan şehir adı alma
+city = st.text_input("Şehir Adı", value="İzmir")
+
+live_data = get_weather_data(city, forecast_type='hourly')
+
+processed_live_data = preprocess_live_data(live_data)
+
+predictions = final_model.predict(processed_live_data)
+
+live_data['predicted_rentals'] = predictions
+
+# Sonuçları ekrana yazdırma
+st.write(live_data[['datetime', 'tempg', 'hum', 'weathersit', 'wind_speedg', 'predicted_rentals']])
+
+
+
 
 
 
